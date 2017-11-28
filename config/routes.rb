100980267c693170ad38b95a20c84c535fb18271
m-rwash <m.rwash93@yahoo.com>
2017-11-28 20:09:21 +0200

    init commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  #match "demo/index", :to => "demo#index", :via => :get
+  #get 'demo/index'
+  
+  #match "/", :to => "demo#index", :via =>"get"
+  #root 'demo/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
